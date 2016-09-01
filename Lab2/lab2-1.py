36553d7d28ddc678968871481311a2a32d80e19c
tommyhj <tommyhj@kth.se>
2016-09-01 22:21:03 +0200

    Update lab2-1.py

--- Lab2/lab2-1.py
+++ Lab2/lab2-1.py
@@ -4,19 +4,16 @@ prisKlass=[30, 28, 25, 24]
 #porto-funktionen beräknar porto utifrån priserna i prisKlass och angiven vikt
 def porto(vikt):
     if vikt<2:
-        pris=prisKlass[0]
-    elif vikt < 6:
-        pris=prisKlass[1]
-    elif vikt < 12:
-        pris=prisKlass[2]
+        return float(vikt)*prisKlass[0]
+    elif vikt<6:
+        return float(vikt)*prisKlass[1]
+    elif vikt<12:
+        return float(vikt)*prisKlass[2]
     else:
-        pris=prisKlass[3]    
-    return float(vikt)*pris
-
-
-print("*"*20, "Portokalkyl", "*"*20, "\n")
+        return float(vikt)*prisKlass[3]    
+    
+print("*"*20, "Portokalkyl 1.0", "*"*20, "\n")
 print("Detta är ett program för att beräkna portokostnaden för paketförsändelser.\n")
 vikt=float(input("Ange paketets vikt i kilo:\n"))
 
 print("\nPaketets porto blir %.2f kr" %porto(vikt))
-
