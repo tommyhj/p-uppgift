c61e5bde7ed3aaea144337dc3cd4b55c18640a67
tommyhj <tommyhj@kth.se>
2016-09-03 10:33:48 +0200

    Update lab2-3.py

--- Lab2/lab2-3.py
+++ Lab2/lab2-3.py
@@ -1,11 +1,9 @@
 antal=int(input("Hur många tal vill du mata in? "))
-i=int(1)
 tal=[]
 
-#While-loopen skapar en lista som heter tal och som bara tar med de positiva talen som användaren skriver in
-while i<=antal:
-    tempVar=int(input("Ange tal %d: " %i)) 
+#For-loopen skapar listan tal med bara de positiva talen
+for i in range(1, antal+1, 1):
+    tempVar=int(input("Ange tal %d: " %i))
     if tempVar>0: tal.append(tempVar)
-    i=i+1
-    
+
 print("\nSumman av de positiva talen blir %d." %sum(tal))
